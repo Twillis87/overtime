0c58a212ab68415f544ff284eb5dd9746f6c7508
twillis <twillis@relevantinc.com>
2019-10-08 14:08:29 -0600

    Add rspec, capybara and setup homepage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,8 +18,5 @@ RSpec.configure do |config|
   config.after(:each) { DatabaseCleaner.clean }
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-end gems in backtraces.
-  config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 end
+
